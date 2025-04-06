17f49d14c9ce42bfc1c299e75faa858fa30d4889
SAUGER Gabriel <gabriel.sauger@loria.fr>
2025-04-06 21:13:39 +0200

    Add -fno-inline option to compile_to_asm.sh

--- compile_to_asm.sh
+++ compile_to_asm.sh
@@ -1,6 +1,6 @@
 # $1: name of the c file to compile to assembly
 # $2 output path
-opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -finline-limit=2"
+opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -fno-inline -Wno-error -finline-limit=2"
 if ! cc -g -Wall -S -masm=intel -I3rd/lua $opt -fPIC --shared "$1" -o "$2" -Iskynet-src; then
 	echo "error compile to asm"
 	exit 1
